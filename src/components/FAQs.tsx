3358b4fbd0e70d0b873ba08ba1184e7e6a7e6cec
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-01-18 07:33:02 +0000

    Scaffolded layout

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -3,7 +3,17 @@ import React from 'react'
 export const FAQs = ({ faqs }) => {
   return (
     <section>
-      <p>The FAQs</p>
+      <h1>The FAQs</h1>
+      {faqs.map((faq, i) => (
+        <FAQ {...faq} key={i} />
+      ))}
     </section>
   )
 }
+
+const FAQ = ({ question, answer }) => (
+  <div>
+    <h2>{question}</h2>
+    <p dangerouslySetInnerHTML={{ __html: answer }} />
+  </div>
+)
