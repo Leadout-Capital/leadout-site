583c8ea23218ad9a8b895543475291b64c749a2f
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-01-22 10:26:50 +0000

    Blog page scaffolded

---
+++ src/pages/blog.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import "../stylesheets/contact.scss";
+import { graphql } from "gatsby";
+
+type BlogProps = {
+  data: {
+    contactFormFields: {
+      edges: QueryNode<ContentfulContactFormField>[]
+    },
+    contactFormTitle: ContentfulTextField,
+    contactFormSubtitle: ContentfulTextField
+  }
+}
+
+const Blog: React.FC<BlogProps> = ({ data }) => {
+  return (
+    <main className={"contact"}>
+      <p>The blog!</p>
+    </main>
+  )
+};
+
+export default Blog;
+
+export const query = graphql`
+  query {
+    contactFormFields: allContentfulContactFormField(
+      filter: { node_locale: { eq: "en-US" } }
+      sort: { fields: [index] }
+    ) {
+      edges {
+        node {
+          key
+          title
+          subtitle
+          required
+          type
+          options
+          airtableTableName
+          airtableColumnName
+        }
+      }
+    }
+    contactFormTitle: contentfulTextField(name: { eq: "Contact Form Title" }) {
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+    contactFormSubtitle: contentfulTextField(name: { eq: "Contact Form Subtitle" }) {
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`;
