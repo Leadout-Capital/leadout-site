a66520823b7fc4cabe1ca3f0cbcbc99030ab354f
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-01-24 08:50:50 +0000

    Updated grid styling

--- src/components/Blog/PostGrid.tsx
+++ src/components/Blog/PostGrid.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { PostCard } from './PostCard';
 
 export const PostGrid: React.FC<{ posts: QueryNode<ContentfulBlogPost>[] }> = ({posts}) => {
-  return <div>{posts.map((post) => <PostCard post={post} />)}</div>;
+  return <div className="post-grid">{posts.map((post) => <PostCard post={post} />)} {posts.map((post) => <PostCard post={post} />)} {posts.map((post) => <PostCard post={post} />)}</div>;
 };
