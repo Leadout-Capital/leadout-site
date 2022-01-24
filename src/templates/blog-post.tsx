30359f2b65fd6c2c78587316391deebe030253f7
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-01-24 14:00:42 +0000

    Added author

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,7 +1,8 @@
 import { CategoryButton } from '../components/Blog/CategoryButton';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import React from 'react';
 import "../stylesheets/post.scss";
+import { format, parseISO } from 'date-fns';
 
 type PostProps = {
   data: {
@@ -10,12 +11,36 @@ type PostProps = {
 }
 
 const blogPost: React.FC<PostProps> = ({ data }) => {
+  const formattedDate = format(parseISO(data.post.date), 'MMMM dd, yyyy');
   return (
     <div className="post">
       <div className="post-wrapper">
         <div className="post-meta">
           <CategoryButton color={data.post.category.color} category={data.post.category.name} url={`/blog/category/${data.post.category.slug}`} />
           <h1>{data.post.title}</h1>
+          <div className="author-details">
+              {data.post.author ? (
+                <Link to={`/blog/author/${data.post.author.slug}`}>
+                  <div className="author-image-wrapper">
+                    <img src={data.post.author.image.file.url} alt={data.post.author.name} />
+                  </div>
+                </Link>
+              ) : (
+                ''
+              )}
+              <div className="author-text">
+                <Link to={`/blog/author/${data.post.author.slug}`}>
+                  <p className="name">{data.post.author.name}</p>
+                </Link>
+                <div className="date-time">
+                  <p className="date">{formattedDate}</p>
+                  <p className="time">
+                    {' '}
+                    ・{data.post.body?.childMarkdownRemark?.timeToRead} min
+                  </p>
+                </div>
+              </div>
+            </div>
         </div>
         <div dangerouslySetInnerHTML={{ __html: data.post.body.childMarkdownRemark.html }} className="post-body"/>
       </div>
