6cd6a062f9fc29f04b42002dada4cc8e500f957d
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-01-24 12:19:27 +0000

    Removed dupes

--- src/components/Blog/PostGrid.tsx
+++ src/components/Blog/PostGrid.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { PostCard } from './PostCard';
 
 export const PostGrid: React.FC<{ posts: QueryNode<ContentfulBlogPost>[] }> = ({posts}) => {
-  return <div className="post-grid">{posts.map((post, i) => <PostCard key={i} post={post} />)} {posts.map((post, i) => <PostCard key={i} post={post} />)} {posts.map((post, i) => <PostCard key={i} post={post} />)}</div>;
+  return <div className="post-grid">{posts.map((post, i) => <PostCard key={i} post={post} />)}</div>;
 };
