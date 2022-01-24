5ad66c9522e18859e1aeb1a4bf36847dc4d82ce6
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-01-24 08:34:51 +0000

    Added category card

--- src/components/Blog/PostCard.tsx
+++ src/components/Blog/PostCard.tsx
@@ -4,58 +4,55 @@ import React from 'react';
 
 export const PostCard: React.FC<{ post: QueryNode<ContentfulBlogPost> }> = ({ post }) => {
   const formattedDate = format(parseISO(post.node.date), 'MMMM dd, yyyy');
-    const author = 'Leadout Capital';
+  const author = 'Leadout Capital';
 
-    return (
-      // <article className="post-card">
-      //   <p>{post.node.title}</p>
-      //   <p>{formattedDate}</p>
-      //   <p>by: {author}</p>
-      //   <p>Excerpt: {post.node.body?.childMarkdownRemark?.excerpt}</p>
-      //   <p>Time to read: {post.node.body?.childMarkdownRemark?.timeToRead}</p>
-      //   <p>Category: {post.node.category?.name} {post.node.category?.color}</p>
-      // </article>
-      <div className="post-card">
-        <div className="inner-card">
-          <article>
-            <div className="text-wrapper">
-              {/* <div className="category-container">
-                <p>Category</p>
-              </div> */}
-              {post.node.title ? (
-                <Link to={`/blog/${post.node.title}`}>
-                  <p className="title">{post.node.title}</p>
+  return (
+    <div className="post-card">
+      <div className="inner-card">
+        <article>
+          <div className="text-wrapper">
+            <div className="category-container">
+              <CategoryButton />
+            </div>
+            {post.node.title ? (
+              <Link to={`/blog/${post.node.title}`}>
+                <p className="title">{post.node.title}</p>
+              </Link>
+            ) : (
+              ''
+            )}
+            {post.node.body?.childMarkdownRemark?.excerpt ? <p className="excerpt">{post.node.body?.childMarkdownRemark?.excerpt}</p> : ''}
+            <div className="author-details">
+              {/* {post.author.thumbnail ? (
+                <Link href={post.author.url}>
+                  <div className="author-image-wrapper">
+                    <Image {...post.author.thumbnail} />
+                  </div>
                 </Link>
               ) : (
                 ''
-              )}
-              {post.node.body?.childMarkdownRemark?.excerpt ? <p className="excerpt">{post.node.body?.childMarkdownRemark?.excerpt}</p> : ''}
-              <div className="author-details">
-                {/* {post.author.thumbnail ? (
-                  <Link href={post.author.url}>
-                    <div className="author-image-wrapper">
-                      <Image {...post.author.thumbnail} />
-                    </div>
-                  </Link>
-                ) : (
-                  ''
-                )} */}
-                <div className="author-text">
-                  {/* <Link href={post.author.url}> */}
-                    <p className="name">{author}</p>
-                  {/* </Link> */}
-                  <div className="date-time">
-                    <p className="date">{formattedDate}</p>
-                    <p className="time">
-                      {' '}
-                      ・{post.node.body?.childMarkdownRemark?.timeToRead} min
-                    </p>
-                  </div>
+              )} */}
+              <div className="author-text">
+                {/* <Link href={post.author.url}> */}
+                  <p className="name">{author}</p>
+                {/* </Link> */}
+                <div className="date-time">
+                  <p className="date">{formattedDate}</p>
+                  <p className="time">
+                    {' '}
+                    ・{post.node.body?.childMarkdownRemark?.timeToRead} min
+                  </p>
                 </div>
               </div>
             </div>
-          </article>
-        </div>
+          </div>
+        </article>
       </div>
-    )
+    </div>
+  )
 };
+
+
+const CategoryButton = () => (
+  <div className="category red"><Link to="/blog/category">Category</Link></div>
+)
