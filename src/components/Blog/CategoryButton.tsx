63d50f27874b856ff4d7a562307972ceada6d3c0
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-03-08 15:58:38 +0000

    Fixed bug with link

--- src/components/Blog/CategoryButton.tsx
+++ src/components/Blog/CategoryButton.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 export const CategoryButton = ({ category, color, url }) => (
-  <div className={`blog-category ${color}`}>
-    <Link to={url}>
-      {category}
-    </Link>
-  </div>
+  <Link to={url} className={`blog-category ${color}`}>
+    {category}
+  </Link>
 )
