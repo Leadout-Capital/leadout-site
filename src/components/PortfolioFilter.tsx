094c86feb32a8fd207615977dd5235208e3fa918
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-04-19 09:26:10 +0100

    Created mobile select menus

--- src/components/PortfolioFilter.tsx
+++ src/components/PortfolioFilter.tsx
@@ -4,7 +4,7 @@ import { Listbox } from '@headlessui/react'
 type PortfolioFilterProps = {
   companies: ContentfulPortfolioCompany[];
   selectedStatus: string;
-  setSelectedStatus: Function;
+  setSelectedStatus: (value: string) => void;
   selectedSector: string;
   setSelectedSector: (value: string) => void;
 }
@@ -32,6 +32,35 @@ export const PortfolioFilter = ({
       <p>Filter by: </p>
       <button onClick={() => setSelectedStatus(undefined)} className={selectedStatus === undefined && 'active'}>All</button>
       {dedupedStatuses.map((status) => <button onClick={() => setSelectedStatus(status)} className={selectedStatus === status && 'active'}>{status}</button>)}
+
+      <div className="select-field-wrapper">
+        <Listbox value={selectedStatus} onChange={setSelectedStatus}>
+          {({open}) => (
+            <>
+              <Listbox.Button className={selectedStatus && 'active'}>{selectedStatus || 'Status'} <span className={`chevron-down ${open ? 'chevron-open' : ''}`} /></Listbox.Button>
+              <Listbox.Options className="options">
+                <Listbox.Option key={0} value={undefined} as={Fragment}>
+                {({ active, selected }) => (
+                  <li className={`${active ? 'active' : ''} ${selected ? 'selected' : ''}`}>All</li>
+                )}
+                </Listbox.Option>
+                {dedupedStatuses.map((sector) => (
+                  <Listbox.Option
+                    key={sector}
+                    value={sector}
+                    as={Fragment}
+                  >
+                    {({ active, selected }) => (
+                      <li className={`${active ? 'active' : ''} ${selected ? 'selected' : ''}`}>{sector}</li>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </>
+          )}
+        </Listbox>
+      </div>
+
 
       <div className="select-field-wrapper">
         <Listbox value={selectedSector} onChange={setSelectedSector}>
