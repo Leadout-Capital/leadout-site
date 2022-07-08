255b50c86ca5b952cb80f8aac6bfe54c745dc2f9
davidfoxio <43332633+davidfoxio@users.noreply.github.com>
2022-07-08 16:27:17 +0100

    Updated logic to handle zero subtitle

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -19,7 +19,7 @@ const blogPost: React.FC<PostProps> = ({ data }) => {
         <div className="post-meta">
           <CategoryButton color={data.post.category.color} category={data.post.category.name} url={`/blog/category/${data.post.category.slug}`} />
           <h1>{data.post.title}</h1>
-          {data.post.subtitle && <h2>{data.post.subtitle}</h2>}
+          {data.post.subtitle && data.post.subtitle !== '0' && <h2>{data.post.subtitle}</h2>}
           <div className="author-details">
               {data.post.author ? (
                 <Link to={`/blog/author/${data.post.author.slug}`}>
